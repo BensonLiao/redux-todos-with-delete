1205fdd5f938534701ccee2fe345517083e92dbb
BensonLiao <skylock777@gmail.com>
2021-01-26 11:44:22 +0800

    refactor(master): renaming component for better readability

--- src/components/FilterButton.js
+++ src/components/FilterButton.js
@@ -2,7 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import {Button} from '@material-ui/core'
 
-const Link = ({ active, children, onClick }) => (
+const FilterButton = ({ active, children, onClick }) => (
     <Button
       variant="outlined"
       color="primary"
@@ -13,10 +13,10 @@ const Link = ({ active, children, onClick }) => (
     </Button>
 )
 
-Link.propTypes = {
+FilterButton.propTypes = {
   active: PropTypes.bool.isRequired,
   children: PropTypes.node.isRequired,
   onClick: PropTypes.func.isRequired
 }
 
-export default Link
+export default FilterButton
--- src/components/TodoListFilter.js
+++ src/components/TodoListFilter.js
@@ -1,20 +1,20 @@
 import React from 'react'
 import {Box} from '@material-ui/core'
-import FilterLink from '../containers/FilterLink'
-import { VisibilityFilters } from '../actions'
+import FilterButton from '../containers/FilterButton'
+import {VisibilityFilters} from '../actions'
 
 const TodoListFilter = () => (
   <Box component='div' m={1}>
     <span>Show: </span>
-    <FilterLink filter={VisibilityFilters.SHOW_ALL}>
+    <FilterButton filter={VisibilityFilters.SHOW_ALL}>
       All
-    </FilterLink>
-    <FilterLink filter={VisibilityFilters.SHOW_ACTIVE}>
+    </FilterButton>
+    <FilterButton filter={VisibilityFilters.SHOW_ACTIVE}>
       Active
-    </FilterLink>
-    <FilterLink filter={VisibilityFilters.SHOW_COMPLETED}>
+    </FilterButton>
+    <FilterButton filter={VisibilityFilters.SHOW_COMPLETED}>
       Completed
-    </FilterLink>
+    </FilterButton>
   </Box>
 )
 
