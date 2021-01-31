f02acf36072e4273f1293d4282144d341ee5e409
BensonLiao <skylock777@gmail.com>
2021-01-31 13:50:30 +0800

    style(tabs): apply with unify material style

--- src/components/RouterTabs.js
+++ src/components/RouterTabs.js
@@ -2,7 +2,7 @@ import React from "react";
 import {withRouter} from "react-router";
 import {Route, Switch, Link} from "react-router-dom";
 import {Paper, Tab, Tabs} from '@material-ui/core';
-import {makeStyles} from "@material-ui/core/styles";
+import {makeStyles, useTheme} from "@material-ui/core/styles";
 import {useAuth} from './App'
 import Home from './Home';
 import Profile from './Profile';
@@ -11,9 +11,18 @@ import VisibleTodoList from '../containers/VisibleTodoList';
 import TodoListFilter from './TodoListFilter';
 
 const useStyles = makeStyles({
-  root: {
+  root: props => ({
     flexGrow: 1,
-  },
+    borderRadius: 4,
+    border: `1px solid ${props.palette.primary.main}`
+  }),
+  tab: props => ({
+    color: props.palette.text.primary,
+    '&:hover': {
+      color: props.palette.primary.main,
+      backgroundColor: props.palette.action.hover
+    }
+  })
 });
 
 const PrivateRoute = ({ component, ...rest }) => {
@@ -37,7 +46,8 @@ const TodoList = () => {
 }
   
 const RouterTabs = withRouter(({location}) => {
-  const classes = useStyles();
+  const theme = useTheme();
+  const classes = useStyles(theme);
   return (
     <>
       <Paper className={classes.root}>
@@ -47,9 +57,9 @@ const RouterTabs = withRouter(({location}) => {
           textColor="primary"
           centered
         >
-          <Tab label="Home" component={Link} to="/home" value='/home'/>
-          <Tab label="Profile" component={Link} to="/profile" value='/profile'/>
-          <Tab label="Todo List" component={Link} to="/todolist" value='/todolist'/>
+          <Tab className={classes.tab} label="Home" component={Link} to="/home" value='/home'/>
+          <Tab className={classes.tab} label="Profile" component={Link} to="/profile" value='/profile'/>
+          <Tab className={classes.tab} label="Todo List" component={Link} to="/todolist" value='/todolist'/>
         </Tabs>
       </Paper>
 
